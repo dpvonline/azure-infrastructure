1e4b6bf1a35d095d5fa59da306e975c2d800a74d
Philip <philiphagemann@icloud.com>
2025-09-07 14:50:18 +0200

    Extend taints of kube system namespace

--- kubernetes/kube_system.tf
+++ kubernetes/kube_system.tf
@@ -2,7 +2,7 @@ resource "kubernetes_namespace" "kube_system" {
   metadata {
     name = "kube-system"
     annotations = {
-      "scheduler.alpha.kubernetes.io/defaultTolerations" = "[{\"Key\": \"kubernetes.azure.com/scalesetpriority\", \"Operator\": \"Equal\", \"Value\": \"spot\", \"Effect\": \"NoSchedule\"},{\"Key\": \"CriticalAddonsOnly\", \"Operator\": \"Exists\"}]",
+      "scheduler.alpha.kubernetes.io/defaultTolerations" = "[{\"Key\": \"CriticalAddonsOnly\", \"Operator\": \"Exists\"}]",
     }
     labels = {
       "control-plane"                  = true,
